020bced76dec462c5f586ee1d39da6ea6a41282b
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-10-31 09:07:08 -0300

    style(atividades): remove seta do arquivo create

--- resources/views/destinos/create.blade.php
+++ resources/views/destinos/create.blade.php
@@ -3,22 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destino</title>
+    <title>Atividade</title>
 </head>
 <body>
-    <h1> Cadastrar Destino</h1>
+    <h1> Cadastrar Atividade</h1>
 
-    <form  action="{{ route('destinos.store') }}" method="post">
+    <form  action="{{ route('atividades.store') }}" method="post">
         @csrf
-        <label for="name">Nome do Destino:</label>
-        <input type="text" name="name" id="name" required>
+        <label for="titulo">Título da Atividade:</label>
+        <input type="text" name="titulo" id="titulo" required>
 
         <label for="descricao">Descrição:</label>
         <input type="text" name="descricao" id="descricao" required>
 
+        <label for="data">Data:</label>
+        <input type="date" name="data" id="data" required>
+        
+        <label for="local">Local:</label>
+        <input type="text" name="local" id="local" required>
+
+        <label for="status">Status:</label>
+        <input type="text" name="status" id="status" required>
+
         <input type="submit" value="Enviar" >
     </form>
 
-    <a href="{{ route('destinos.index') }}">← Voltar para lista</a>
+    <a href="{{ route('atividades.index') }}"> Voltar para lista</a>
 </body>
 </html>
