5fcb9946a50573d358d883bd9b27dda9c04f29a7
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-10-31 09:05:26 -0300

    style(atividades): ajusta texto do h1 e remove seta

--- resources/views/destinos/edit.blade.php
+++ resources/views/destinos/edit.blade.php
@@ -3,23 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destino</title>
+    <title>Atividade</title>
 </head>
 <body>
-    <h1> Cadastrar Destino</h1>
+    <h1> Atualizar Atividade</h1>
 
-   <form id="destino-form" action="{{ route('destinos.update', $destino->id) }}" method="post">
+     <form action="{{ route('atividades.update', $atividade->id) }}" method="post">
         @csrf
         @method("put")
-        <label for="name">Nome do Destino:</label>
-        <input type="text" name="name" id="name"  value="{{ $destino->name }}" required>
+        <label for="titulo">Título da Atividade:</label>
+        <input type="text" name="titulo" id="titulo" value="{{ $atividade->titulo }}" required>
 
         <label for="descricao">Descrição:</label>
-        <input type="text" name="descricao" id="descricao"  value="{{ $destino->descricao }}"required>
+        <input type="text" name="descricao" id="descricao"  value="{{ $atividade->descricao }}" required>
+
+        <label for="data">Data:</label>
+        <input type="date" name="data" id="data" value="{{ $atividade->data }}" required>
+        
+        <label for="local">Local:</label>
+        <input type="text" name="local" id="local" value="{{ $atividade->local }}" required>
+
+        <label for="status">Status:</label>
+        <input type="text" name="status" id="status" value="{{ $atividade->status }}" required>
 
         <input type="submit" value="Enviar" >
     </form>
 
-    <a href="{{ route('destinos.index') }}">← Voltar para lista</a>
+    <a href="{{ route('atividades.index') }}"> Voltar para lista</a>
 </body>
 </html>
