d1b8ecf6e87c18aea8a00b12d7ac5bdfda0de5fa
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-10-31 09:00:11 -0300

    feat(atividades): adiciona rota resource de atividades

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinoController;
+use App\Http\Controllers\AtividadeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('destinos', DestinoController::class);
+
+Route::resource('atividades', AtividadeController::class);
