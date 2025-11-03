4364f00d37a7271b539b6fbd82ff57a1dd6950a9
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-03 16:55:09 -0300

    feat(orcamentos): criar controller para gerenciar orçamentos

---
+++ app/Http/Controllers/OrcamentoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orcamento;
+use Illuminate\Http\Request;
+
+class OrcamentoController extends Controller
+{
+   
+     public function index()
+    {
+        $orcamentos = Orcamento::all();
+        return view("orcamentos.index" , compact("orcamentos"));
+    }
+
+    public function create()
+    {
+      return view('orcamentos.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'titulo' => 'required|string|max:255',
+        'valorEstimado' => 'required|numeric',
+        'valorGasto' => 'required|numeric',
+        'descricao' => 'required|string|max:255',
+    ]);
+
+        Orcamento::create($request->all());
+
+        return redirect()->route('orcamentos.index')->with('success', 'Orçamento criada com sucesso!');
+    }
+
+    public function edit(Orcamento $orcamento)
+    {
+        return view('orcamentos.edit', compact('orcamento'));
+    }
+
+    public function update(Request $request, Orcamento $orcamento)
+    {
+        $orcamento->update($request->all());
+        return redirect()->route('orcamentos.index')->with('success', 'Orçamento atualizada com sucesso!');
+    }
+
+    public function destroy(Orcamento $orcamento)
+    {
+        $orcamento->delete();
+        return redirect()->route('orcamentos.index')->with('success', 'Orçamento excluída com sucesso!');
+    }
+}
