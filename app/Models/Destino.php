24a3bf49b05a214574613c0346b01f6bb70ba6d4
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-12 13:10:17 -0300

    feat: relaciona Destino com Orçamento

--- app/Models/Destino.php
+++ app/Models/Destino.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Destino extends Model
 {
     protected $fillable = ["name", "descricao"];
+
+    public function orcamentos()
+    {
+        return $this->hasMany(Orcamento::class, 'id_destino');
+    }
 }
--- app/Models/Orcamento.php
+++ app/Models/Orcamento.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Orcamento extends Model
 {
     protected $fillable = ["titulo", "valorEstimado", "valorGasto", "descricao"];
+
+    public function destino()
+    {
+        return $this->belongsTo(Destino::class, 'id_destino');
+    }
 }
