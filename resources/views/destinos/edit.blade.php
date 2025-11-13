55b7db115beedf8b88aa27a82fa60dae7829db9e
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-13 12:56:35 -0300

    fix: corrige formulário do edit

--- resources/views/destinos/edit.blade.php
+++ resources/views/destinos/edit.blade.php
@@ -3,32 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade</title>
+    <title>Destino</title>
 </head>
 <body>
-    <h1> Atualizar Atividade</h1>
+    <h1> Cadastrar Destino</h1>
 
-     <form action="{{ route('atividades.update', $atividade->id) }}" method="post">
+    <form  action="{{ route('destinos.update', $destino->id) }}" method="post">
         @csrf
         @method("put")
-        <label for="titulo">Título da Atividade:</label>
-        <input type="text" name="titulo" id="titulo" value="{{ $atividade->titulo }}" required>
+        <label for="name">Nome do Destino:</label>
+        <input type="text" name="name" id="name" value="{{ $destino->name }}" required>
 
         <label for="descricao">Descrição:</label>
-        <input type="text" name="descricao" id="descricao"  value="{{ $atividade->descricao }}" required>
-
-        <label for="data">Data:</label>
-        <input type="date" name="data" id="data" value="{{ $atividade->data }}" required>
-        
-        <label for="local">Local:</label>
-        <input type="text" name="local" id="local" value="{{ $atividade->local }}" required>
-
-        <label for="status">Status:</label>
-        <input type="text" name="status" id="status" value="{{ $atividade->status }}" required>
+        <input type="text" name="descricao" id="descricao" value="{{ $destino->descricao }}" required>
 
         <input type="submit" value="Enviar" >
     </form>
 
-    <a href="{{ route('atividades.index') }}"> Voltar para lista</a>
+    <a href="{{ route('destinos.index') }}"> Voltar para lista</a>
 </body>
 </html>
