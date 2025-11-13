d19ed98c417008f49b292ed8a1fc96eaf385a854
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-13 14:10:48 -0300

    refactor: remove campo título

--- app/Models/Orcamento.php
+++ app/Models/Orcamento.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Orcamento extends Model
 {
-    protected $fillable = ["titulo", "valorEstimado", "valorGasto", "descricao", "id_destino"];
+    protected $fillable = ["valorEstimado", "valorGasto", "descricao", "id_destino"];
 
     public function destino()
     {
