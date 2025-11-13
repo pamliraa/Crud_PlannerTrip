aa990e6872a849e570f66067c20a1efaba838c42
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-13 14:34:54 -0300

    refactor: retira campo título e permite inserir números decimais nos campos de valor

--- resources/views/orcamentos/create.blade.php
+++ resources/views/orcamentos/create.blade.php
@@ -10,25 +10,23 @@
 
     <form  action="{{ route('orcamentos.store') }}" method="post">
         @csrf
-        <label for="titulo">Título:</label>
-        <input type="text" name="titulo" id="titulo" required>
+        
+        <label for="id_destino">Destino</label>
+        <select name="id_destino" id="id_destino">
+            <option value="">Selecione um destino</option>
+            @foreach($destinos as $destino)
+                <option value="{{ $destino->id }}">{{ $destino->name }}</option>
+            @endforeach
+        </select>
 
         <label for="valorEstimado">Valor Estimado:</label>
-        <input type="number" name="valorEstimado" id="valorEstimado" required>
+        <input type="number" step="0.01" name="valorEstimado" id="valorEstimado" required>
         
         <label for="valorGasto">Valor Gasto:</label>
-        <input type="number" name="valorGasto" id="valorGasto" required>
+        <input type="number" step="0.01" name="valorGasto" id="valorGasto" required>
 
         <label for="descricao">Descrição:</label>
         <input type="text" name="descricao" id="descricao" required>
-
-        <label for="id_destino">Destino relacionado:</label>
-        <select name="id_destino">
-            <option value="">Selecione um destino</option>
-            @foreach($destinos as $destino)
-                <option value="{{ $destino->id }}">{{ $destino->nome }}</option>
-            @endforeach
-        </select>
 
         <input type="submit" value="Enviar" >
     </form>
