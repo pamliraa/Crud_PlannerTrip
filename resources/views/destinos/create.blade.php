dae983aaa080d01e74fc16d7f54a3b206ddaf2de
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-17 13:12:54 -0300

    style: adiciona placeholders nos campos de destinos

--- resources/views/destinos/create.blade.php
+++ resources/views/destinos/create.blade.php
@@ -10,13 +10,13 @@
 
     <form  action="{{ route('destinos.store') }}" method="post">
         @csrf
-        <label for="name">Nome do Destino:</label>
-        <input type="text" name="name" id="name" required>
+    <label for="name">Nome do Destino:</label>
+    <input type="text" name="name" id="name" placeholder="Ex.: Paris" required>
 
-        <label for="descricao">Descrição:</label>
-        <input type="text" name="descricao" id="descricao" required>
+    <label for="descricao">Descrição:</label>
+    <input type="text" name="descricao" id="descricao" placeholder="Ex.: Capital da França, famosa pela Torre Eiffel e museus" required>
 
-        <input type="submit" value="Enviar" >
+    <input type="submit" value="Enviar" >
     </form>
 
     <a href="{{ route('destinos.index') }}"> Voltar para lista</a>
