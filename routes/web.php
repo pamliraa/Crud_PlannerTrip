776fd8207a7221643f54f11a3867c6e0eb84646e
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-18 13:07:06 -0300

    feat: adiciona rota para crud de diario

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DestinoController;
 use App\Http\Controllers\AtividadeController;
 use App\Http\Controllers\OrcamentoController;
+use App\Http\Controllers\DiarioController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,3 +15,5 @@ Route::resource('destinos', DestinoController::class);
 Route::resource('atividades', AtividadeController::class);
 
 Route::resource('orcamentos', OrcamentoController::class);
+
+Route::resource('diarios', DiarioController::class);
