b05f1493639beae9e8af5b2cd0dc95acec4fa49d
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-11-18 13:07:54 -0300

    feat: cria migrate de diario

---
+++ database/migrations/2025_11_18_153610_create_diarios_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('diarios', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->text('descricao');
+            $table->string('foto')->nullable();
+            $table->foreignId('destino_id')->constrained('destinos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diarios');
+    }
+};
