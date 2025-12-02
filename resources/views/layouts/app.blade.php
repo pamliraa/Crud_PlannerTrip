0798b53b561fc97e33a92715b09bf5ac41e61f5a
Pamela Lira <pamela.saldanha1@aluno.ce.gov.br>
2025-12-02 16:54:00 -0300

    style: estiliza a cor da nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="font-sans antialiased" style="background-color:#e4ebf0;">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
